39f4890e71b2e95d5b0010dc522625061d0794fa
LiamEdwards931 <>
2023-06-18 15:14:41 +0000

    set up functions for game structure

---
+++ assets/javascript/script.js
@@ -0,0 +1,27 @@
+function runGame();{
+
+}
+function correctAnswer();{
+
+}
+function incrementScore();{
+
+}
+function incrementWrongAnswer();{
+
+}
+function displayAdditionQuestion();{
+
+}
+function displayAdditionQuestion();{
+
+}
+function displaySubtractionQuestion();{
+
+}
+function displayMulitplyQuestion();{
+
+}
+function displayDivideQuestion();{
+
+}
