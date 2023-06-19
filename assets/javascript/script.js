2438d2f94181a550c66c8614466eab95ad654334
LiamEdwards931 <>
2023-06-19 19:09:17 +0000

    add code to generate 2 random numbers

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,10 +15,15 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-
-function runGame(){
-
+/**The main game "loop", called when the script is first loaded, 
+ * and after a users answer has been processed.
+ */
+function runGame() {
+    //creates 2 random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25 + 1);
+    let num2 = Math.floor(Math.random() * 25 +1);
 }
+
 function correctAnswer(){
 
 }
