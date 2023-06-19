bdf911f07e478c7adb0c660d5bba3e706fe89e6d
LiamEdwards931 <>
2023-06-19 18:59:48 +0000

    add event listener for when buttons are click

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,27 +1,45 @@
-function runGame();{
+//event listener for when the DOM loads
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for(let button of buttons){
+        button.addEventListener("click", function() {
+            if(this.getAttribute("data-type")=== "submit") {
+                alert("You Clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`you clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+
+function runGame(){
 
 }
-function correctAnswer();{
+function correctAnswer(){
 
 }
-function incrementScore();{
+function incrementScore(){
 
 }
-function incrementWrongAnswer();{
+function incrementWrongAnswer(){
 
 }
-function displayAdditionQuestion();{
+function displayAdditionQuestion(){
 
 }
-function displayAdditionQuestion();{
+function displayAdditionQuestion(){
 
 }
-function displaySubtractionQuestion();{
+function displaySubtractionQuestion(){
 
 }
-function displayMulitplyQuestion();{
+function displayMulitplyQuestion(){
 
 }
-function displayDivideQuestion();{
+function displayDivideQuestion(){
 
 }
