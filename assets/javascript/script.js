dc78992a2ab019f39f2a4811b2c157d600358e5a
LiamEdwards931 <>
2023-06-19 19:27:04 +0000

    add addition question function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,19 +9,28 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You Clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`you clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+    runGame("addition");
 });
 
 /**The main game "loop", called when the script is first loaded, 
  * and after a users answer has been processed.
  */
-function runGame() {
+function runGame(gameType) {
     //creates 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25 + 1);
     let num2 = Math.floor(Math.random() * 25 +1);
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
 function correctAnswer(){
@@ -36,12 +45,18 @@ function incrementWrongAnswer(){
 function displayAdditionQuestion(){
 
 }
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
+
 function displaySubtractionQuestion(){
 
 }
+
 function displayMulitplyQuestion(){
 
 }
